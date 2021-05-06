ba3b6abfcc5e0633a0fc82c1e7c3dae9f9070c3c
babdurrahman78 <babdurrahman78@gmail.com>
2021-05-06 07:04:10 +0700

    close #2

--- pages/mypokemon/index.js
+++ pages/mypokemon/index.js
@@ -1,8 +1,19 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import { css } from "@emotion/react";
-import Image from 'next/image';
-import { MyPokemonContext } from '../../components/PokemonContext';
+import { MyPokemonContext } from "../../components/PokemonContext";
 import { Card, CardTitle, CardImg, Row, Col, Button } from "reactstrap";
+
+const pEmpty =css`
+          text-align: center;
+          font-size: 20px;
+          font-weight: 500;
+         `;
+
+const pEmpty1=css`
+         text-align: center;
+         font-size: 20px;
+         font-weight: 500;
+        `
 
 const row = css`
   max-width: 1000px;
@@ -42,83 +53,82 @@ const nickname = css`
 `;
 
 const name = css`
-  color: #A9A9A9;
+  color: #a9a9a9;
   text-align: center;
-`
+`;
 
 const MyPokemon = () => {
 
-
   const [state, dispatch] = useContext(MyPokemonContext);
-  console.log(state);
-  const remove = (ev) =>{
+
+
+
+  const remove = (ev) => {
     dispatch({
       type: "DELETE_POKEMON",
-      payload: ev.currentTarget.value
+      payload: ev.currentTarget.value,
     });
-  }
-
+  };
 
   return (
     <div>
-      {state.pokemons.length === 0 && <div css={css`
-          min-width: 300px;
-          max-width: 400px;
-          margin: 100px auto 0;
-        `}>
-          <img src='/poke.jpg' alt='poke-logo' css={css`
-            width: 200px;
-            display: block;
-            height: auto;
-            margin: auto
-          `}
-           />
-           <p css={css`
-            text-align: center;
-            font-size: 20px;
-            font-weight: 500;
-           `}>0 Pokemon owned.</p>
-           <p css={css`
-            text-align: center;
-            font-size: 20px;
-            font-weight: 500;
-           `}>Go Catch some Pokemons!</p>
-        </div>}
       <Row css={row} className="justify-content-between">
-      {state.pokemons && state.pokemons.map((pokemon) => {
-        return (
-          <Col
-            css={col}
-            className="col-6 col-xs-4 col-sm-3 col-md-3 col-lg-3 col-xl-4 mt-2"
-            key={pokemon.nickname}
-          >
-            <a css={a}>
-              <Card css={card} body>
-                <CardImg
-                  css={img}
-                  top
-                  width="100%"
-                  src={pokemon.image}
-                  alt={pokemon.name}
-                />
-                <CardTitle css={nickname} tag="h5">
-                  {pokemon.nickname.charAt(0).toUpperCase() + pokemon.nickname.slice(1)}
-                </CardTitle>
-                <CardTitle css={name} tag="h5">
-                  {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-                </CardTitle>
-                <Button color="danger" value={pokemon.nickname} onClick={remove}>
-                  Delete
-                </Button>
-              </Card>
-            </a>
-          </Col>
-        );
-      })}
-    </Row>
+      {state.pokemons.length === 0 && <>
+        <p css={css`
+          margin-top: 100px;
+          width: 100%;
+          text-align: center;
+          font-size: 20px;
+          font-weight: 500;
+         `}>0 Pokemon owned.</p>
+         <p css={css`
+          margin-top: -20px;
+          width: 100%;
+          text-align: center;
+          font-size: 20px;
+          font-weight: 500;
+         `}>Go catch some pokemon!</p>
+        </>}
+        {state.pokemons &&
+          state.pokemons.map((pokemon) => {
+            return (
+              <Col
+                css={col}
+                className="col-6 col-xs-4 col-sm-3 col-md-3 col-lg-3 col-xl-4 mt-2"
+                key={pokemon.nickname}
+              >
+                <a css={a}>
+                  <Card css={card} body>
+                    <CardImg
+                      css={img}
+                      top
+                      width="100%"
+                      src={pokemon.image}
+                      alt={pokemon.name}
+                    />
+                    <CardTitle css={nickname} tag="h5">
+                      {pokemon.nickname.charAt(0).toUpperCase() +
+                        pokemon.nickname.slice(1)}
+                    </CardTitle>
+                    <CardTitle css={name} tag="h5">
+                      {pokemon.name.charAt(0).toUpperCase() +
+                        pokemon.name.slice(1)}
+                    </CardTitle>
+                    <Button
+                      color="danger"
+                      value={pokemon.nickname}
+                      onClick={remove}
+                    >
+                      Delete
+                    </Button>
+                  </Card>
+                </a>
+              </Col>
+            );
+          })}
+      </Row>
     </div>
-
-      );
+  );
 };
 
 export default MyPokemon;
